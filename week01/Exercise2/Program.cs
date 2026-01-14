c0395463a02b5b7b4a48f5245df55840715742c9
camocros <sernaarciniegas@gmail.com>
2026-01-13 20:47:12 -0700

    0.1

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Whats is your grade percentage.");
+        string ValuePercentage = Console.ReadLine();
+        int grade = int.Parse(ValuePercentage);
+        
+string letter;
+
+if (grade >= 90)
+{
+    letter = "A";
+}
+else if (grade >= 80)
+{
+    letter = "B";
+}
+else if (grade >= 70)
+{
+    letter = "C";
+}
+else if (grade >= 60)
+{
+    letter = "D";
+}
+else
+{
+    letter = "F";
+}
+    
+        Console.WriteLine($"Your letter grade is: {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Don't give up—keep trying and you'll do better next time!");
+        }
     }
 }
